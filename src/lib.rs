e89d0c3329bdfb48f04323400d98d6624cde7d82
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2024-11-06 07:20:46 -0500

    readd validate userid to trait, add more unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
-pub mod validator;
 pub mod mapper;
+pub mod validator;
--- src/validator/validator.rs
+++ src/validator/validator.rs
@@ -3,12 +3,17 @@ use super::types::{V1UserInformation, V2UserInformation};
 pub trait Validator {
     fn validate_v1(&self, data: &V1UserInformation) -> bool;
     fn validate_v2(&self, data: &V2UserInformation) -> bool;
+    fn valid_userid(&self, id: i64) -> bool;
 }
 
 pub struct ModelValidator;
 impl Validator for ModelValidator {
     fn validate_v1(&self, data: &V1UserInformation) -> bool {
-        if data.id.to_string().len() != 16 || data.id.to_string().chars().nth(0).unwrap() != '2' {
+        // if data.id.to_string().len() != 16 || data.id.to_string().chars().nth(0).unwrap() != '2' {
+        //     eprintln!("ID is empty or the incorrect format, dropping record");
+        //     return false;
+        // }
+        if !self.valid_userid(data.id) {
             eprintln!("ID is empty or the incorrect format, dropping record");
             return false;
         }
@@ -24,11 +29,32 @@ impl Validator for ModelValidator {
     }
 
     fn validate_v2(&self, data: &V2UserInformation) -> bool {
-        if data.id.to_string().len() != 16 || data.id.to_string().chars().nth(0).unwrap() != '2' {
+        // if data.id.to_string().len() != 16 || data.id.to_string().chars().nth(0).unwrap() != '2' {
+        //     eprintln!("ID is empty or the incorrect format, dropping record");
+        //     return false;
+        // }
+        if !self.valid_userid(data.id) {
             eprintln!("ID is empty or the incorrect format, dropping record");
             return false;
         }
+        if data.account_information.registered.is_empty() {
+            eprintln!("Registered is empty, dropping record");
+            return false;
+        }
+        if data.user_information.first_name.is_empty() || data.user_information.last_name.is_empty()
+        {
+            eprintln!("First name and Last name are empty, dropping record");
+            return false;
+        }
+        if data.contact_information.email.is_empty() {
+            eprintln!("Email is empty, dropping record");
+            return false;
+        }
         true
+    }
+
+    fn valid_userid(&self, id: i64) -> bool {
+        id.to_string().len() == 16 && id.to_string().chars().nth(0).unwrap() == '2'
     }
 }
 
@@ -41,18 +67,28 @@ impl Validator for ModelValidator {
 mod tests {
     use super::*;
 
-    // Tests for V1UserInformation
+    // **************************** test userid ***************************** //
+    #[test]
+    fn test_valid_userid() {
+        let validator = ModelValidator;
+        assert_eq!(validator.valid_userid(2377983216433421), true);
+    }
+
+    #[test]
+    fn test_invalid_userid() {
+        let validator = ModelValidator;
+        assert_eq!(validator.valid_userid(1234567890), false);
+    }
+
+    // **************************** v1 Schema ******************************* //
     #[test]
     fn test_validate_v1() {
-        // validate the V1UserInformation struct against Correct data
-        // -- SHOULD PASS --
         let data = V1UserInformation::default();
         let validator = ModelValidator;
         assert_eq!(validator.validate_v1(&data), true);
     }
 
     #[test]
-    // #[should_panic(expected = "ID is empty or the incorrect format, dropping record")]
     fn test_validate_v1_id() {
         let mut data = V1UserInformation::default();
         data.id = 1234567890;
@@ -61,7 +97,6 @@ mod tests {
     }
 
     #[test]
-    // #[should_panic(expected = "Name is empty, dropping record")]
     fn test_validate_v1_name() {
         let mut data = V1UserInformation::default();
         data.name = String::new();
@@ -70,7 +105,6 @@ mod tests {
     }
 
     #[test]
-    // #[should_panic(expected = "Email is empty, dropping record")]
     fn test_validate_v1_email() {
         let mut data = V1UserInformation::default();
         data.email = String::new();
@@ -78,6 +112,51 @@ mod tests {
         assert_eq!(validator.validate_v1(&data), false);
     }
 
-    // Tests for V2UserInformation
-    // soon....
+    // **************************** v2 Schema ******************************* //
+    #[test]
+    fn test_validate_v2() {
+        let data = V2UserInformation::default();
+        let validator = ModelValidator;
+        assert_eq!(validator.validate_v2(&data), true);
+    }
+
+    #[test]
+    fn test_validate_v2_id() {
+        let mut data = V2UserInformation::default();
+        data.id = 1234567890;
+        let validator = ModelValidator;
+        assert_eq!(validator.validate_v2(&data), false);
+    }
+
+    #[test]
+    fn test_validate_v2_registered() {
+        let mut data = V2UserInformation::default();
+        data.account_information.registered = String::new();
+        let validator = ModelValidator;
+        assert_eq!(validator.validate_v2(&data), false);
+    }
+
+    #[test]
+    fn test_validate_v2_firstname() {
+        let mut data = V2UserInformation::default();
+        data.user_information.first_name = String::new();
+        let validator = ModelValidator;
+        assert_eq!(validator.validate_v2(&data), false);
+    }
+
+    #[test]
+    fn test_validate_v2_lastname() {
+        let mut data = V2UserInformation::default();
+        data.user_information.last_name = String::new();
+        let validator = ModelValidator;
+        assert_eq!(validator.validate_v2(&data), false);
+    }
+
+    #[test]
+    fn test_validate_v2_email() {
+        let mut data = V2UserInformation::default();
+        data.contact_information.email = String::new();
+        let validator = ModelValidator;
+        assert_eq!(validator.validate_v2(&data), false);
+    }
 }
